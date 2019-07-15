c805f06cb901b35446c325019416ab83c24250df
ZastrixArundell <zastrixarundell@gmail.com>
2019-07-15 13:28:31 +0200

    Initial commit

---
+++ src/main/java/com/github/zastrixarundell/shortestapi/ShortestAPI.java
@@ -0,0 +1,105 @@
+package com.github.zastrixarundell.shortestapi;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+
+
+public class ShortestAPI
+{
+
+    private String link;
+    private String token;
+    private String responseAsJson, shortenedURL, statusMessage;
+
+    public ShortestAPI(String token)
+    {
+        this.token = token;
+    }
+
+    public ShortestAPI(String token, String link)
+    {
+        this.token = token;
+        this.link = link;
+    }
+
+    public void sendRequest() throws IOException
+    {
+        link = "urlToShorten=" + link;
+
+        URL url = new URL("https://api.shorte.st/v1/data/url");
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestMethod("PUT");
+        con.setRequestProperty("Host", "api.shorte.st");
+        con.setRequestProperty("User-Agent", "curl/7.52.1");
+        con.setRequestProperty("Accept", "*/*");
+        con.setRequestProperty("public-api-token", token);
+        con.setRequestProperty("Content-Length", String.valueOf(link.length()));
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setDoOutput(true);
+        con.setDoInput(true);
+
+        OutputStream stream = con.getOutputStream();
+        stream.write(link.getBytes());
+
+        InputStream input = con.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        stream.flush();
+        stream.close();
+
+        responseAsJson = reader.readLine();
+
+        JsonObject jsonObject = new Gson().fromJson(responseAsJson, JsonObject.class);
+
+        statusMessage = jsonObject.get("status").getAsString();
+        shortenedURL = jsonObject.get("shortenedUrl").getAsString();
+
+        input.close();
+
+        con.disconnect();
+    }
+
+    public String getReponseJson()
+    {
+        return responseAsJson;
+    }
+
+    public void setLink(String link)
+    {
+        this.link = link;
+    }
+
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    public String getLink()
+    {
+        return link;
+    }
+
+    public String getToken()
+    {
+        return token;
+    }
+
+    public String getResponseAsJson()
+    {
+        return responseAsJson;
+    }
+
+    public String getShortenedURL()
+    {
+        return shortenedURL;
+    }
+
+    public String getStatusMessage()
+    {
+        return statusMessage;
+    }
+}
